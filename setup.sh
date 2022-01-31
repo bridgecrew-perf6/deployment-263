3256bd19c2f774073b810d165ec19f949a528a19
sumukshashidhar <sumukuuu@gmail.com>
2022-01-30 20:12:54 -0600

    feat[setup]: add certbot

--- setup.sh
+++ setup.sh
@@ -9,6 +9,13 @@ sudo apt install docker-compose
 # make nginx html directory
 sudo mkdir -p /usr/share/nginx/html
 
+# add certbot stuff
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt update
+sudo apt install certbot
 
+# make a certificate
+sudo certbot certonly --standalone -d blog.uiucquant.web.illinois.edu
 
 sudo docker-compose up
